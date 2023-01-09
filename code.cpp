c5305eecc8e9ca3b5a5f7ddbbcef9742a405d483
Lavanya Tulsian <tulsian2lavanya@gmail.com>
2023-01-09 15:24:11 +0530

    this is commit from lavanya

--- code.cpp
+++ code.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 
 int main(){
+    char a;
+    cout<<"enter name of user: ";
+    cin>>a;
+    cout<<"the user name is: "<<a<<endl;
     cout << "Hello world" << endl;
+    return 0;
 }
